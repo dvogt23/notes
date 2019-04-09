679a538c687bd8678cee9e483f2ca79bd71b0588
dvogt23 <divogt@live.de>
2019-04-09 21:36:10 +0100

    sync file

--- sync.sh
+++ sync.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+NOTES_DIR="/mnt/usb/ncdata/dvogt/files/Notes"
+cd /home/pi/notes
 rsync -rtvu --delete --exclude='private' $NOTES_DIR . --out-format='%f'
+
+git pull
+git add Notes/*
+git commit -m "Update" # TODO: commit message what updated
+git push
