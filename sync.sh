ac167617cb70e25bf15d8d82d889536faabfb50d
dvogt23 <divogt@live.de>
2021-01-12 07:46:55 +0000

    mod sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+git pull origin master
+
 NOTES_DIR="/mnt/usb/ncdata/dvogt/files/Notes"
 cd /mnt/usb/notes
-rsync -rtvu --delete --exclude='private' --exclude='diary' --exclude='html' $NOTES_DIR . --out-format='%f'
+rsync -rtvu --delete --exclude='private' --exclude='diary' --exclude='html' --exclude='work' $NOTES_DIR . --out-format='%f'
 
-git pull
 git add Notes/*
 git commit -m "Update" # TODO: commit message what updated
 git push
