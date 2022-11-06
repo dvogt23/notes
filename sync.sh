837893f0dc11b971b0f3ce960cc7c08fd0f9fc17
dvogt23 <divogt@live.de>
2022-11-06 21:41:35 +0100

    build: Add yml-header preprocessor

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-git pull origin master
+git pull origin main
 
-rsync -rtvu --delete --exclude='private' --exclude='diary' --exclude='html' --exclude='work' $NOTES_DIR . --out-format='%f'
+rsync -rtvu --delete --exclude='private' --exclude='diary' --exclude='html' --exclude='.obsidian' --exclude='work' $NOTES_DIR . --out-format='%f'
 
 git add Notes/*
 git commit -m "$(git --no-pager diff --name-only --diff-filter=AM)"
