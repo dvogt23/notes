1daf4cf7ec3aa8e8474f2fcb115150ee7b5a1aee
dvogt23 <divogt@live.de>
2023-04-13 09:43:49 +0200

    ignore from file

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 git pull origin main
-rsync -rtvu --delete --exclude='private' --exclude='diary' --exclude='html' --exclude='.obsidian' --exclude='work' $NOTES_DIR . --out-format='%f'
+rsync -rtvu --delete --exclude-from='.ignore_sync' $NOTES_DIR . --out-format='%f'
 
 git add Notes/*
 git commit -m "$(git --no-pager diff --name-only --cached --diff-filter=AM)"
