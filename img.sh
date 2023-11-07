8b9d9ddec9eecb2d1586fc96c3405cbd39084af2
dvogt23 <divogt@live.de>
2023-11-07 11:11:14 +0100

    feat: Add last-changed

--- img.sh
+++ img.sh
@@ -2,5 +2,10 @@
 
 cd "$(dirname "$0")"
 
-convert $1 -alpha set -virtual-pixel transparent \
-	-channel A -blur 0x8 -level 50%,100% +channel $1
+for file in $(git diff --cached --name-only | grep ".[jpg|jpeg|png]\$"); do
+	echo "Crop $file"
+	convert ${file} -alpha set -virtual-pixel transparent \
+		-channel A -blur 0x8 -level 50%,100% +channel ${file}.new
+	# rm -f $file
+	# mv -f ${file}.new $file
+done
