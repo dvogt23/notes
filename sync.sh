b08bdc585a6f1cb532c2e40dacc8b3bd57b53f97
dvogt23 <divogt@vogt.dev>
2025-06-11 08:24:44 +0200

    update sync
    test

--- sync.sh
+++ sync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
 
 git pull origin main
-rsync -rtvu --delete --exclude-from='.ignore_sync' $NOTES_DIR . --out-format='%f'
+rsync -rtvu --delete --exclude-from='.ignore_sync' "$NOTES_DIR" . --out-format='%f'
 
 git add Notes/*
-git commit -m "$(git --no-pager diff --name-only --cached --diff-filter=AM)"
+git commit -m "Auto-update" -m '' -m "$(git --no-pager diff --name-only --cached --diff-filter=AM)"
 git push
