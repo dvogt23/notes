4d96a69f9256e665cc7029c16e16893a2b22aee3
dvogt23 <dvogt23@users.noreply.github.com>
2026-04-27 22:00:55 +0000

    deploy: 47d2e50441c7c25218b4b5c0001ab887febbccc5

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-80e915fe.js',
+                path_to_root + 'searchindex-e59eb057.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
